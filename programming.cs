20f8e08491bf66b0bfa09e445d70f2cba65808e2
mahapajosias <khwinanatebogo22@gmail.com>
2026-04-13 22:05:20 +0200

    Refactor Main method in Program class
    Removed unnecessary comments and simplified the Main method.

--- programming.cs
+++ programming.cs
@@ -7,16 +7,13 @@ class Program
         Console.Title = "Cybersecurity Awareness Bot";
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-        // Play voice (optional - remove if you don't have AudioPlayer)
-        // AudioPlayer.PlayGreeting();
-
-        // Show logo
+    
         UIHelper.ShowLogo();
 
-        // Start chatbot
+        
         Chatbot bot = new Chatbot();
         bot.Start();
 
-        // No need for the extra while loop - the menu handles everything
+    
     }
 }
